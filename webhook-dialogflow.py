24daba70b1d7eda3ede66c110a2b7013b93f6a60
nandai-at-fa <mohican.buggy@gmail.com>
2019-06-20 14:46:23 +0900

    JSONキーを安全な取扱へ変更

--- webhook-dialogflow.py
+++ webhook-dialogflow.py
@@ -23,8 +23,24 @@ import gspread
 
 #OAuth 2.0で保護されたリソースにアクセスするためのライブラリ
 from oauth2client.service_account import ServiceAccountCredentials
+
 #scope = ['https://spreadsheets.google.com/feeds']
 scope = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
+
+# 辞書オブジェクト。認証に必要な情報をHerokuの環境変数から呼び出している
+credential = {
+                "type": "service_account",
+                "project_id": os.environ['SHEET_PROJECT_ID'],
+                "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
+                "private_key": os.environ['SHEET_PRIVATE_KEY'],
+                "client_email": os.environ['SHEET_CLIENT_EMAIL'],
+                "client_id": os.environ['SHEET_CLIENT_ID'],
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_x509_cert_url":  os.environ['SHEET_CLIENT_X509_CERT_URL']
+             }
+
 
 #現在日時のdatetimeオブジェクトを取得するためのライブラリ
 from datetime import datetime
@@ -60,7 +76,7 @@ def processRequest(req):
     print(depth)
 
     #ダウンロードしたjsonファイルを同じフォルダに格納して指定する
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('probing-test-project-2049bc1f32a8.json', scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential, scope)
     gc = gspread.authorize(credentials)
 
     # 共有設定したスプレッドシートの名前を指定する
