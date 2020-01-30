7fd429afc9749ea4b9b98002526d99294d49469f
nandai-at-fa <mohican.buggy@gmail.com>
2020-01-30 16:02:34 +0900

    if else文のコロンわすれ

--- webhook-dialogflow.py
+++ webhook-dialogflow.py
@@ -94,14 +94,14 @@ def processRequest(req):
     jst_now = utc_now.astimezone(timezone('Asia/Tokyo'))
 
     # シートに行を追加して記入
-    if not len( depth ) == 0
+    if not len( depth[0] ) == 0:
         worksheet.append_row([jst_now.strftime("%Y/%m/%d %H:%M:%S"),tooth_number[0],virtical[0],depth_val[0]]);
         worksheet.append_row([jst_now.strftime("%Y/%m/%d %H:%M:%S"),tooth_number[0],virtical[0],depth_val1[0]]);
-    else
+    else:
         worksheet.append_row([jst_now.strftime("%Y/%m/%d %H:%M:%S"),tooth_number[0],horizontal[0],virtical[0],depth[0]]);
 
-    if not len( depth_val2 ) == 0
-    else
+    if not len( depth_val2[0] ) == 0:
+    else:
         worksheet.append_row([jst_now.strftime("%Y/%m/%d %H:%M:%S"),tooth_number[0],virtical[0],depth_val2[0]]);
 
 if __name__ == '__main__':
